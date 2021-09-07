8790c131ed96d95c0f9c69c9a22ef266b6a1c0c2
Aaron Hamilton <aaronleon28@gmail.com>
2021-09-07 15:45:11 +0100

    Loading Fullscreen Window (EOC)
    We had created code to make the screen load up in full at 1080p.
    Fixed Wrong location of files.
    
    EOC = End Of Class

--- Timber/Timber/main.cpp
+++ Timber/Timber/main.cpp
@@ -1,6 +1,9 @@
-#include "SFML/Graphics.hpp"
+#include "include/SFML/Graphics.hpp"
 
 int main() 
 {
+	sf::VideoMode vm(1920, 1080);
+	sf::RenderWindow window(vm, "Timber", sf::Style::Fullscreen);
+
 	return 0;
 }
