782431fcdc875f306eff61d00c7724fcc83a66fb
Aaron Hamilton <aaronleon28@gmail.com>
2021-09-07 16:35:24 +0100

    Loading the background (GDE)
    We had made the game now load the background with the sprite.
    
    GDE = GitHub Desktop Error

--- Timber/Timber/main.cpp
+++ Timber/Timber/main.cpp
@@ -9,6 +9,29 @@ int main()
 	sf::RenderWindow window(vm, "Timber", sf::Style::Fullscreen);
 	#pragma endregion
 
+
+	#pragma region Load Background Image
+	/*Create a texture to hold the background*/
+	sf::Texture bacGroTex;
+	/*Link the image to the texture background*/
+	bacGroTex.loadFromFile("assets/graphics/background.png");
+	#pragma endregion
+
+	#pragma region Load Sprite for background
+	/*Creating a sprite to displace the background*/
+	sf::Sprite bacGroSpr;
+	/*Linking the sprite to the texture*/
+	bacGroSpr.setTexture(bacGroTex);
+	#pragma endregion
+
+	#pragma region Location of Sprite
+	/*Seeing the location of the sprite(0,0)*/
+	bacGroSpr.setPosition(0, 0);
+	#pragma endregion
+
+
+
+
 	#pragma region While Window Is Open
 	while (window.isOpen()) 
 	{
@@ -23,9 +46,10 @@ int main()
 
 		#pragma region Effecting the scene
 		/*Update the scene*/
-		/*Draw the scene*/
 		/*Clear the scene*/
 		window.clear();
+		/*Draw the scene*/
+		window.draw(bacGroSpr);
 		/*Render the scene*/
 		window.display();
 		#pragma endregion
@@ -33,8 +57,6 @@ int main()
 
 	}
 #pragma endregion
-
-
 
 	//Return 0 will tell the program nothing bad happened and can end
 	return 0;
